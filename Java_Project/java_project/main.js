f39851eef5df088a9adc5a7249bf7d3827564e57
surjiun <53025066+surjiun@users.noreply.github.com>
2020-11-11 12:11:52 +0900

    카테고리칸

--- Java_Project/java_project/main.js
+++ Java_Project/java_project/main.js
@@ -12,17 +12,19 @@ ca_toogleB.addEventListener('click',()=> {
 ki_toogleB.addEventListener('click',()=>{
   toggleKind[0].classList.toggle("show");
 });
-function SetGridItemHeight(){
-  let grid = document.getElementsByClassName('grid')[0];
-  let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
 
-  let item = grid.getElementsByClassName('item');
-  for(let i =0; i < item.length;++i){
-      item[i].style.gridRowEnd = `span ${Math.floor((item[i].children[0].offsetHeight)/25)}`
-      // Math.ceil -> Math.floor
-      // item[i].offsetHeight -> item[i].children[0].offsetHeight
-  }
+
+function SetGridItemHeight(){
+	let grid = document.getElementsByClassName('grid')[0];
+	let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+	let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+
+	let item = grid.getElementsByClassName('item');
+	for(let i =0; i < item.length;++i){
+		item[i].style.gridRowEnd = `span ${Math.ceil((item[i].offsetHeight)/(rowHeight + rowGap))}`;
+	}
+
+
 }
 
 window.addEventListener("load", SetGridItemHeight);
