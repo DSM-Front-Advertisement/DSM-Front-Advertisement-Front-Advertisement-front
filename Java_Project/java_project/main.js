ec45e816988fd2cfb8a898fc9facd7fc9e597ba4
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-11-16 11:17:47 +0900

    2020.11.16 코드 정렬

--- Java_Project/java_project/main.js
+++ Java_Project/java_project/main.js
@@ -1,7 +1,7 @@
-const ca_toogleB = document.querySelector('.menubar_categorytoogle');
-const ki_toogleB = document.querySelector('.menubar_kindbartogle');
-const kat = document.querySelector('.Kategorie_bar');
-const kind = document.querySelector('.kind_bar');
+const ca_toogleB = document.querySelector(".menubar_categorytoogle");
+const ki_toogleB = document.querySelector(".menubar_kindbartogle");
+const kat = document.querySelector(".Kategorie_bar");
+const kind = document.querySelector(".kind_bar");
 const toggleCategory = document.getElementsByClassName("category-toggle");
 const toggleKind = document.getElementsByClassName("kind-toggle");
 const imgInput = document.getElementById("image");
@@ -9,29 +9,30 @@ const vidInput = document.getElementById("video");
 const file = document.getElementsByClassName("file_ad");
 const link = document.getElementsByClassName("link_ad");
 console.log(1);
-ca_toogleB.addEventListener('click',()=> {
+ca_toogleB.addEventListener("click", () => {
   toggleCategory[0].classList.toggle("show");
 });
 console.log(2);
-ki_toogleB.addEventListener('click',()=>{
+ki_toogleB.addEventListener("click", () => {
   toggleKind[0].classList.toggle("show");
 });
 
+function SetGridItemHeight() {
+  let grid = document.getElementsByClassName("grid")[0];
+  let rowHeight = parseInt(
+    window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
+  );
+  let rowGap = parseInt(
+    window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
+  );
 
-
-
-function SetGridItemHeight(){
-	let grid = document.getElementsByClassName('grid')[0];
-	let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-	let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-
-	let item = grid.getElementsByClassName('item');
-	for(let i =0; i < item.length;++i){
-		item[i].style.gridRowEnd = `span ${Math.ceil((item[i].offsetHeight)/(rowHeight + rowGap))}`;
-	}
+  let item = grid.getElementsByClassName("item");
+  for (let i = 0; i < item.length; ++i) {
+    item[i].style.gridRowEnd = `span ${Math.ceil(
+      item[i].offsetHeight / (rowHeight + rowGap)
+    )}`;
+  }
 }
-
-
 
 window.addEventListener("load", SetGridItemHeight);
 window.addEventListener("resize", SetGridItemHeight);
