b63adbd1dfa78c84fd745d0f29f2ae4a0e077442
LeeSeongJinCa <dltjdqhr55@gmail.com>
2020-12-01 17:11:06 +0900

    시청 기록 완료

--- history/history.js
+++ history/history.js
@@ -5,19 +5,59 @@ const IMAGE_BASE_URL =
 const apiDefault = axios.create({
   baseURL: BASE_URL,
 });
-
-const historyListWrap = document.getElementById("historyListWrap");
-
 const histories = JSON.parse(localStorage.getItem("histories"));
 
-function itemTemplate(ids) {
+const historyListWrap = document.getElementById("historyListWrap");
+const zoomWrap = document.getElementById("zoomWrap");
+const youtubeCopy = document.getElementById("youtubeCopy");
+
+function zoomIn(type, link, subLink) {
+  const imageWrap = document.getElementById("zoomImageWrap");
+  const youtubeWrap = document.getElementById("zoomYoutubeWrap");
+
+  zoomWrap.style.display = "block";
+
+  if (type === "image") {
+    const image = document.getElementById("zoomImage");
+    const subLinkWrap = document.getElementById("zoomImageSubLink");
+    const download = document.getElementById("download");
+
+    imageWrap.style.display = "flex";
+    youtubeWrap.style.display = "none";
+    image.setAttribute("src", `${IMAGE_BASE_URL}${link}`);
+    subLinkWrap.textContent = subLink;
+    subLinkWrap.setAttribute("href", subLink);
+    download.dataset.id = link;
+  } else if (type === "youtube") {
+    const youtube = document.getElementById("zoomYoutube");
+    const subLinkWrap = document.getElementById("zoomYoutubeSubLink");
+    const youtubeLink = document.getElementById("youtubeLink");
+
+    youtubeWrap.style.display = "block";
+    imageWrap.style.display = "none";
+    youtube.setAttribute("src", `https://www.youtube.com/embed/${link}`);
+    subLinkWrap.textContent = subLink;
+    subLinkWrap.setAttribute("href", subLink);
+    youtubeLink.value = `https://www.youtube.com/watch?v=${link}`;
+  }
+}
+
+function zoomOut() {
+  zoomWrap.style.display = "none";
+}
+
+function itemTemplate(items) {
   let template = "";
 
-  console.log(ids.length);
+  for (let i = 0; i < items.length; i++) {
+    const id = items[i].advertisementId;
+    const ytId = items[i].youtubeLink;
+    let item = "";
 
-  for (let i = 0; i < 3 && i < ids.length; i++) {
-    const item = `<li class="item">
-      <img src="${IMAGE_BASE_URL}${ids[i]}" alt="${ids[i]}" title="${ids[i]}" />
+    if (items[i].mediaType === 1) {
+      // ? youtube
+      item = `<li class="item" onclick="zoomIn('youtube', '${ytId}', '${items[i].subLink}')">
+      <img src="https://img.youtube.com/vi/${ytId}/0.jpg" alt="${ytId}" title="${ytId}" />
       <div class="hovered">
         <img
           src="../assets/round-play-button.svg"
@@ -26,6 +66,19 @@ function itemTemplate(ids) {
         />
       </div>
     </li>`;
+    } else {
+      // ? image
+      item = `<li class="item" onclick="zoomIn('image', '${id}', '${items[i].subLink}')">
+      <img src="${IMAGE_BASE_URL}${id}" alt="${id}" title="${id}" />
+      <div class="hovered">
+        <img
+          src="../assets/round-play-button.svg"
+          alt="play button"
+          title="play button"
+        />
+      </div>
+    </li>`;
+    }
 
     template += item;
   }
@@ -34,7 +87,7 @@ function itemTemplate(ids) {
 }
 
 function listTemplate(history) {
-  const { date, ids } = history;
+  const { date, items } = history;
 
   const template = `<div class="list">
     <div class="itemLeft">
@@ -43,15 +96,22 @@ function listTemplate(history) {
         <span>시청</span>
       </p>
       <ul class="itemWrap">
-        ${itemTemplate(ids)}
+        ${itemTemplate(items)}
       </ul>
-    </div>
-    <div class="itemRight">
-      <p><span>${ids.length}</span>개 더보기</p>
     </div>
   </div>`;
 
   return template;
+}
+
+function download(e) {
+  const id = e.target.dataset.id;
+  const a = document.createElement("a");
+
+  if (id === "0") return alert("다운로드에 실패헸습니다.");
+
+  a.href = `${IMAGE_BASE_URL}${id}`;
+  a.click();
 }
 
 function initialize() {
@@ -59,5 +119,14 @@ function initialize() {
     historyListWrap.insertAdjacentHTML("beforeend", listTemplate(history));
   });
 }
+
+youtubeCopy.addEventListener("click", () => {
+  const youtubeLink = document.getElementById("youtubeLink");
+
+  youtubeLink.select();
+
+  document.execCommand("copy");
+  alert("클립보드 복사 완료!");
+});
 
 window.addEventListener("load", initialize);
